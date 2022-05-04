b2b6f15b92f215ff8c21ac4ca3d8fe63a0386320
PhilippMartinBosch <philipp.bosch@uni-konstanz.de>
2022-05-04 11:06:40 +0200

    clean package script

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -4,15 +4,7 @@
 suppressWarnings(if (!require("pacman")) install.packages("pacman"))
 
 # load packages and install if not installed --------------------------------------------
-<<<<<<< Updated upstream
 pacman::p_load(tidyverse, broom, readr, here,
-=======
-<<<<<<< Updated upstream
-pacman::p_load(tidyverse, broom, readr, here
-=======
-pacman::p_load(tidyverse, broom,
->>>>>>> Stashed changes
->>>>>>> Stashed changes
                install = TRUE,
                update = FALSE)
 
